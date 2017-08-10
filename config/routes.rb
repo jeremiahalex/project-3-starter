f49b0dcd456d0c981fd357e431b651b22f599b74
lee brian <leesinshingbrian@gmail.com>
2017-08-10 12:59:47 +0800

    edited some setup

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
               sign_up: 'register'
             }
 
-  resources :posts
+  # resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
