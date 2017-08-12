bfe00fe996b123da868ad346cc0955054072f3be
Dominic Phua <dominikphua@gmail.com>
2017-08-12 11:41:18 +0800

    Installed newest gems and jquery

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809125507) do
+ActiveRecord::Schema.define(version: 20170810044838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "description"
+    t.string "date"
+    t.string "time"
+    t.string "pax"
+    t.string "something"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
