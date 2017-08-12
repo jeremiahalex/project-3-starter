16a199fc70cd4193452fe4315cc5b69e393ba36e
Dominic Phua <dominikphua@gmail.com>
2017-08-12 13:24:21 +0800

    Working models

---
+++ db/migrate/20170812050226_create_deals.rb
@@ -0,0 +1,12 @@
+class CreateDeals < ActiveRecord::Migration[5.1]
+  def change
+    create_table :deals do |t|
+      t.string :name
+      t.text :description
+      t.integer :pax
+      t.references :restaurant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809125507) do
+ActiveRecord::Schema.define(version: 20170812051631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "pax"
+    t.bigint "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_deals_on_restaurant_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "pax"
+    t.date "date"
+    t.time "time"
+    t.bigint "deal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deal_id"], name: "index_groups_on_deal_id"
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_groups_users_on_group_id"
+    t.index ["user_id"], name: "index_groups_users_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "latitude"
+    t.integer "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -33,4 +68,6 @@ ActiveRecord::Schema.define(version: 20170809125507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "restaurants"
+  add_foreign_key "groups", "deals"
 end
