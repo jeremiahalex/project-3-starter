8e572358aaf5f902cf29af129684ae3b736d7d34
mmmlll <michelle.y.lai@gmail.com>
2017-08-14 17:54:29 +0800

    scraping UI and endpoints done

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
   resources :events
   resources :items
 
+  get '/shoppinglist', to: 'shoppinglist#start'
+  post '/shoppinglist', to: 'shoppinglist#results'
+  post '/shoppinglist/basket', to: 'shoppinglist#basket'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
