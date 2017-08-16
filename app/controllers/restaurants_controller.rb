1b51ae737d56fde8cfae25e4f8fed331f4e1c4a0
Ernest Tan <evilernie1985@gmail.com>
2017-08-16 14:29:24 +0800

    Search by restaurant name feature implemented on views/restaurants/main

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,13 @@ class RestaurantsController < ApplicationController
 
   def index
     all_restaurants = Restaurant.all
+    #search by restaurant name
+    if params[:search]
+      all_restaurants = Restaurant.search(params[:search]).order("created_at DESC")
+    else
+      all_restaurants = Restaurant.all.order("created_at DESC")
+    end
+
     all_deals =
       all_restaurants.map do |restaurant|
         restaurant.deals
@@ -12,5 +19,14 @@ class RestaurantsController < ApplicationController
 
     render json: {restaurants: all_restaurants, deals: all_deals}
   end
+
+  def search
+  @restaurants = Restaurant.all
+  if params[:search]
+    @restaurant = Resraurant.search(params[:search]).order("created_at DESC")
+  else
+    @restaurant = Restaurant.all.order("created_at DESC")
+  end
+end
 
 end
