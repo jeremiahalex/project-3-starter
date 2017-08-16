5a59c9aa4b0089488751fd152f0ee42bc9f5ad30
Nicholas Lee <niconal902@gmail.com>
2017-08-16 17:12:40 +0800

    can now search and add

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170810092207) do
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "location"
     t.date "date"
     t.time "time"
