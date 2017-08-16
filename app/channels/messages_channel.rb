e7602c1b0a8759ce5ff4c90df0ab1d0161731a85
yangtheng <yangtheng92@gmail.com>
2017-08-16 19:18:16 +0800

    action cables working but cant save in database

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -4,7 +4,6 @@ class MessagesChannel < ApplicationCable::Channel
   end
 
   def receive(payload)
-    message = Message.create(chatroom_id: payload["chatroom_id"], content: payload["message"])
-    ActionCable.server.broadcast('messages', {message: message.content, chatroom_id: message.chatroom_id})
+    Message.create(user: current_user, chatroom_id: params["room"], content: payload["message"])
   end
 end
