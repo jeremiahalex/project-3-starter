dae12b3e74afad6e5ffeaa04ca14c1bcfe119284
yangtheng <yangtheng92@gmail.com>
2017-08-17 11:02:13 +0800

    set rails host in heroku configu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = '/cable'
-  config.action_cable.url = 'wss://stormy-dawn-37243.herokuapp.com/cable'
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   config.action_cable.allowed_request_origins = ['https://stormy-dawn-37243.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
