067f3f99588aa24ba90abd238662854d7a62903f
Wanzhen Ng <ngwanzhen@gmail.com>
2017-08-17 14:32:18 +0800

    uncessary routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # resources :items
 
 get '/items/new', to: 'items#new'
-post '/items/new', to: 'items#new'
+# post '/items/new', to: 'items#new'
 post '/items', to: 'items#results'
 post '/items/create', to: 'items#create'
 delete '/items/:id', to: 'items#destroy', as: 'items_delete'
