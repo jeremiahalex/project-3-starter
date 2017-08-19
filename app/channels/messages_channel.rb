46ffed95ccc070b103e39aee7c3067b6c4a4998f
yangtheng <yangtheng92@gmail.com>
2017-08-19 14:29:56 +0800

    added connected message to chat, added sound alert when others send you a message

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,17 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'messages'
+    ActionCable.server.broadcast "messages", {message: "<p><strong>#{current_user.name}</strong> has connected</p>", chatroom_id: params[:room]}
+  end
+
+  def unsubscribed
+    ActionCable.server.broadcast "messages", {message: "<p><strong>#{current_user.name}</strong> has disconnected</p>", chatroom_id: params[:room]}
   end
 
   def receive(payload)
-    Message.create(user_id: payload["user_id"], chatroom_id: payload["chatroom_id"], content: payload["message"])
+    if Chatroom.find(payload["chatroom_id"]).group.users.include? current_user
+      Message.create(user: current_user, chatroom_id: payload["chatroom_id"], content: payload["message"])
+    end
   end
+
 end
