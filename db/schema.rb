4f8b716519f4cdbc21110f78056cf4486d666fac
noobnur <nurhsans@gmail.com>
2017-11-03 21:26:12 +0800

    added booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103114114) do
+ActiveRecord::Schema.define(version: 20171103120623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.time "time"
+    t.date "date"
+    t.text "venue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "languages", force: :cascade do |t|
     t.text "name"
