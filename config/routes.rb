99d068a3f980c984af7383950a4e43eb29ba78e7
noobnur <nurhsans@gmail.com>
2017-11-06 14:34:14 +0800

    added users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
 
-  resources :users
+  get 'users/create'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
