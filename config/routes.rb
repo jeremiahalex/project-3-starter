0d7d13afa50422f29188f495cc210e74bb23cc44
siya-ng <ngsiya90@gmail.com>
2017-11-07 15:34:28 +0800

    devise change routes name -siya

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'static_pages#home'
   get  'static_pages/home'
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login', sign_out: 'logout',
+     sign_up: 'register' }
+  resources :users, only: [:index]
+  get '/users/:name', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
