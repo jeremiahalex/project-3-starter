848f2639e453cd52ce8f035f8cbf7ab85128ff76
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-11-07 14:51:36 +0800

    index updated with login and register

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#home'
-  
-  get 'pages/home'
+  devise_for :users
 
-  get 'users/index'
-
-  get 'users/create'
-
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/show'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
+  # get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
