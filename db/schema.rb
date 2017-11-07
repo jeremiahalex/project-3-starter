fab2f8f6021a671637d3ad27632804606bb8b9bd
noobnur <nurhsans@gmail.com>
2017-11-07 16:07:27 +0800

    added name field in register

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20171107061146) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "photo"
