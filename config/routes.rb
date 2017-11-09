8227b83cdae3988e75f976aee244604134a80961
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-11-09 14:05:23 +0800

    edit route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: {
     sign_in: 'login', sign_out: 'logout',
-     sign_up: 'register' }
+    sign_up: 'register' }
   resources :users, only: [:index]
-  # get '/users/:name', to: 'users#show'
+  get '/users/:name', to: 'users#show'
+  get 'profile', to: 'devise/registrations#edit'
+
 
   resources :children
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
