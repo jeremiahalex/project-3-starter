15c6b2139a13649a15fda4cc5ee0678f0cde22aa
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-11-09 10:50:23 +0800

    landing page - hazel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :street, :unit, :postalcode])
-    update_attrs = [:name, :phone, :street, :unit, :postalcode, :password, :password_confirmation, :current_password]
-    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   end
 
 end
