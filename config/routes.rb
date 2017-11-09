17669de25850b7be81c27ecf8f08b292d8676899
hello <31865878+mingyikoh@users.noreply.github.com>
2017-11-09 16:12:17 +0800

    change children route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     sign_up: 'register'
   }
 
-  resources :children
+  resources :children, except: [:edit, :new]
+  get 'profile/children/edit', to: 'children#edit'
+  get 'profile/children/new', to: 'children#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
