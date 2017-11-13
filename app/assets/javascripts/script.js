d764a70b1b889e19e4e2099f7169d374d731880e
Chong Qiang Chew <chewchongqiang@gmail.com>
2017-11-13 14:45:08 +0800

    added checkbox toggle for tutor

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,13 +1,26 @@
+$(function() {
 
-    function rotateCard(btn){
-        var $card = $(btn).closest('.card-container');
-        console.log($card);
-        if($card.hasClass('hover')){
-            $card.removeClass('hover');
-        } else {
-            $card.addClass('hover');
-        }
+  function rotateCard(btn){
+    var $card = $(btn).closest('.card-container');
+    console.log($card);
+    if($card.hasClass('hover')){
+      $card.removeClass('hover');
+    } else {
+      $card.addClass('hover');
     }
-    $('.picker').datetimepicker({
-     step : 5
-    })
+  }
+
+  var $checkbox = $('#test_id')
+  var $isTutor_div = $('.is_tutor')
+  $checkbox.on('change', function() {
+    if(this.checked) {
+      $isTutor_div.css('display', 'block')
+    } else {
+      $isTutor_div.css('display', 'none')
+    }
+  })
+
+
+
+
+})
