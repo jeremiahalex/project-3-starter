f361d0ff735a0b5b90a6d7ccc08553f65c23764a
Chong Qiang Chew <chewchongqiang@gmail.com>
2017-11-13 14:47:22 +0800

    moved rotateCard function out of another function

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,17 +1,7 @@
 $(function() {
-
-  function rotateCard(btn){
-    var $card = $(btn).closest('.card-container');
-    console.log($card);
-    if($card.hasClass('hover')){
-      $card.removeClass('hover');
-    } else {
-      $card.addClass('hover');
-    }
-  }
-
   var $checkbox = $('#test_id')
   var $isTutor_div = $('.is_tutor')
+
   $checkbox.on('change', function() {
     if(this.checked) {
       $isTutor_div.css('display', 'block')
@@ -20,7 +10,14 @@ $(function() {
     }
   })
 
-
-
-
 })
+
+function rotateCard(btn){
+  var $card = $(btn).closest('.card-container');
+  console.log($card);
+  if($card.hasClass('hover')){
+    $card.removeClass('hover');
+  } else {
+    $card.addClass('hover');
+  }
+}
