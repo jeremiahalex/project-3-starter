9ab6d0b83e974ad76dec8629939da4c16c4a373d
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-11-14 17:25:15 +0800

    nav bar edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :street, :unit, :postalcode])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :street, :unit, :postalcode])
   end
-
+  
   private
 
-  
+
 
 end
