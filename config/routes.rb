e4ea1ce10fae1540c245e6fde79f61980513a5bf
siya-ng <ngsiya90@gmail.com>
2017-11-15 15:37:54 +0800

    add browse page

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,7 @@ Rails.application.routes.draw do
   get '/choose_size_style/:size/:style', to: 'filter_form#form2'
   get '/choose_size_style/:size/:style/browse', to: 'clothes#index'
   resources :clothes, only: [:destroy]
+
+  get '/browse', to: 'browse#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
