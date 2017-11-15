f33024f290fc210c18933ffd6070931666e5d14c
hello <31865878+mingyikoh@users.noreply.github.com>
2017-11-15 18:28:18 +0800

    merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :street, :unit, :postalcode])
   end
 
-
   private
 
 end
