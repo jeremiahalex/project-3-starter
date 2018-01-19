e5c9f388d0ee21465c4c37fa951292bf776e9509
chongct <chongchingtong@gmail.com>
2018-01-19 22:57:52 +0800

    rspec, css framework setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def login
+  end
+
+  def search
+  end
+
+  def profile
+  end
+
+  def result
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/login'
+
+  get 'pages/search'
+
+  get 'pages/profile'
+
+  get 'pages/result'
+
+  get 'pages/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
