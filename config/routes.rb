810ef8573f2279d1a8afb1c2f8308f36363dac26
Edmund Teo <Edmund@edmunds-mac.mbox.com>
2018-01-24 14:58:19 +0800

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
 
+  root 'pages#home'
+  resources :pages
+  devise_for :users,
+  path: '',
+  path_names: {
+  sign_in: 'login',
+  sign_out: 'logout',
+  password: 'secret',
+  confirmation: 'verification',
+  unlock: 'unblock',
+  registration: 'register',
+  sign_up: ''
+}
   # get '/login', to: 'pages#login'
   #
   # get '/search', to: 'pages#search'
