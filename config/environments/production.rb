e9d9aa0f869aee4b015dd32cafec1d5b8a5a9a50
Darren Koh <darren212@gmail.com>
2018-01-25 12:30:14 +0800

    Production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For Mailer
+  config.action_mailer.default_url_options = { host: 'warranted.herokuapp.com'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: Rails.application.secrets.GMAIL_USERNAME,
+  password: Rails.application.secrets.GMAIL_PASSWORD
+  }
+
 end
