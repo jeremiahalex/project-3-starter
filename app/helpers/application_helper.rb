db0ba545b69e73f9a58ccb9cdb19023e780a939c
Edmund Teo <Edmund@edmunds-mac.mbox.com>
2018-01-31 10:17:42 +0800

    changing of company or user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def online_status(user)
-  content_tag :span, user.username,
+    naming = user.space ? user.space.company_name : user.username
+  content_tag :span, naming,
               class: "user-#{user.id} online_status #{'online' if user.online?}"
 end
 end
