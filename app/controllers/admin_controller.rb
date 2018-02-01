719029c30da1ebe3d253a2108f201b7febbd440a
Darren Koh <darren212@gmail.com>
2018-02-01 18:52:03 +0800

    List all warranties for Admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,9 @@
 class AdminController < ApplicationController
   before_action :is_admin?
   skip_before_action :verify_authenticity_token
+
+  def index
+    @warranty_list = Warranty.all
+    # render json: @warranty_list
+  end
 end
--- app/models/warranty.rb
+++ app/models/warranty.rb
@@ -3,16 +3,4 @@ class Warranty < ApplicationRecord
   belongs_to :product
   validates :product_id, uniqueness: true
 
-  def list_products
-    warranty_list = self.warranty.where(customer_id: current_user.id)
-    product_list = []
-    warranty_list.each do |e|
-      product = {
-        'name' => Product.find(e.product_id).name,
-        'status' => Product.find(e.product_id).name,
-        'end_date' => Product.find(e.product_id).name
-      }
-      product_list << product
-    end
-  end
 end
